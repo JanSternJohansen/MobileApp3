188ac61cdf49fbc1bd94eb26ce09c2d3674718db
JanSternJohansen <jaj@tec.dk>
2021-01-27 08:45:40 +0100

    Navigation færdig

--- NavigatePage/app/Second/second-page.ts
+++ NavigatePage/app/Second/second-page.ts
@@ -1,6 +1,22 @@
-import { EventData } from "@nativescript/core";
+import { EventData, Page, TextField } from "@nativescript/core";
+import { MyData } from "~/MyData";
+
+var myData: MyData;
+var page: Page;
 
 export function onNavigatingTo(args: EventData)
 {
+    page = <Page>args.object;
+    myData = page.navigationContext.myData;
+    var par1: string = page.navigationContext.param1;
 
+    var txtFromMain: TextField = page.getViewById("txtFromMain");
+    txtFromMain.text = myData.Forward;
+}
+
+export function onGoMain(args: EventData)
+{
+    var txtToMain: TextField = page.getViewById("txtToMain");
+    myData.Backward = txtToMain.text;
+    page.frame.goBack();
 }
